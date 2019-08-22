4097702feb9ae9b0ff2a1cc2207eef672361c4c1
Mark Leo Cativo <cativomarkleo@gmail.com>
2019-08-22 21:33:59 +0800

    used generic type(K extends keyof T) in get method

--- web/src/models/Attributes.ts
+++ web/src/models/Attributes.ts
@@ -1,8 +1,12 @@
 export class Attributes<T> {
   constructor(private _data: T) {}
 
-  get(propName: string): string | number {
-    return this._data[propName];
+  //   yung laman ng K ay mga key ng T,
+  //   kunware pag yung UserProps interface yung ginamit,
+  // id, name age yung mga type sa K
+  //   yung pwedeng value lang nung key ay mga key ng T
+  get<K extends keyof T>(key: K): T[K] {
+    return this._data[key];
   }
 
   set(updates: T): void {
