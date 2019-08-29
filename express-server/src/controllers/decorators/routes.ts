a6a2cfea9546ac6815d0ae45a3d097488846c870
Mark Leo Cativo <cativomarkleo@gmail.com>
2019-08-29 18:30:43 +0800

    added property descriptor for type checking

--- express-server/src/controllers/decorators/routes.ts
+++ express-server/src/controllers/decorators/routes.ts
@@ -1,10 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import 'reflect-metadata';
+import { RequestHandler } from 'express';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
 
+// para sure na may routehandler bago magamit yung routeBinder
+// hindi yung kung anu-anong method lang
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
+
 const routeBinder = (httpMethod: string) => function factoryDecorator(path: string) {
-  return function attachMetadata(target: any, propertyKey: string): void {
+  return function attachMetadata(
+    target: any,
+    propertyKey: string,
+    desc: RouteHandlerDescriptor,
+  ): void {
     Reflect.defineMetadata(MetadataKeys.Path, path, target, propertyKey);
     Reflect.defineMetadata(MetadataKeys.HttpMethod, httpMethod, target, propertyKey);
   };
